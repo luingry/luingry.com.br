99afc6992b344a3f6d71a7a3f8011fb9f8ed1748
Luingry <luingry@gmail.com>
2017-12-05 23:06:16 -0200

    aprimoramento easteregg + pulse

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
   if($(".logo").hasClass("cheat-enabled")){
   	$(".logo").removeClass("cheat-enabled");
+  	$(".logo").addClass("pulse");
   }else{
+  	$(".logo").removeClass("pulse");
   	$(".logo").addClass("cheat-enabled");
+  	;
   }
 });
